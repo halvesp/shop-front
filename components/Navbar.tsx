d187131e18f57d4441d31868ce385ea81a25f895
henrique.apires <henrique.apires@outlook.com>
2024-05-18 22:10:15 -0300

    logout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,26 @@
+import { useRouter } from "next/router";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+
 export default function Navbar() {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    router.push("/login");
+  };
+
   return (
     <div className="flex items-center text-zinc-100 w-full drop-shadow fixed z-40">
-      <div className="bg-background w-full p-4 px-8 flex gap-5">
-        <img src="/logo2.png" width={170} />
+      <div className="bg-background w-full p-4 px-8 flex justify-between items-center">
+        <img src="/logo2.png" width={170} alt="Logo" />
+        <Button
+          onClick={handleLogout}
+          className="bg-background border-0 text-secondary flex items-center gap-2 text-md hover:bg-accent"
+        >
+          Sair
+          <LogOut width={20} />
+        </Button>
       </div>
     </div>
   );
